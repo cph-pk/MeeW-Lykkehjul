288f9d8684dd7e4950851bd5ccbcf047117f8e10
Per <cph-pk171@cphbusiness.dk>
2021-04-28 13:12:50 +0200

    Added Wheel component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,47 @@
-export default function Home() {
+import React, { useState } from "react";
+import Wheel from "./Wheel";
+import { Button, Container, Row, Col, Modal } from 'react-bootstrap';
 
-    return (
-        <div>
-            <h2>Home</h2>
-        </div>
-    )
+export default function Home() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+
+
+  return (
+    <Container fluid="lg">
+      <Row>
+        <Col>
+
+          <div>
+            <h2>Lykkehjul demo</h2>
+            <p>
+              Her er en simpel test af et lykkehjul i en modal.
+          </p>
+
+          </div>
+
+          <Button variant="primary" onClick={handleShow}>
+            Launch demo wheel
+          </Button>
+
+          <Modal contentClassName="modal" show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+              <Modal.Title>Modal heading</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              Woohoo, you're reading this text in a modal!
+              <div>
+                <Wheel />
+
+              </div>
+            </Modal.Body>
+
+          </Modal>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
